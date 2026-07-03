23ba12a95000068e652afa98656fbaa4ce455a95
Yutaka Takahashi <speedstar11010118@gmail.com>
2026-07-03 23:21:46 +0900

    feat: 認証機能の実装 #3

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\Category;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +15,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/login');
+});
+
+// 疎通確認の仮ルート
+Route::middleware('auth')->group(function () {
+    Route::get('/admin', fn() => '管理画面（準備中）');
 });
